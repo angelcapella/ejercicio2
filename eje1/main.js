8ca78b6ba5f8aedfd353a7bf22a0413ad80a87c7
DESKTOP-G7I7M5O\kape9 <angelcapellaruiz@gmail.com>
2020-09-25 13:16:46 +0200

    eje1

--- eje1/main.js
+++ eje1/main.js
@@ -1,8 +1,8 @@
 function nombres(x) {
-    x.surname = 'Doe';
+    
 }
 
-let array = [{'name':'pepe'},{'name':'juan'}];
+let array = [{'name':'ana'},{'name':'javi'},{'name':'roberto'},{'name':'vanessa'}];
 array.forEach(nombres);
 console.log(array[0]);
 console.log(array[1]);
