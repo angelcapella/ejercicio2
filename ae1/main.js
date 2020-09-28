54e50618d545f7f002b64364c8e9a4f4d99c050a
DESKTOP-G7I7M5O\kape9 <angelcapellaruiz@gmail.com>
2020-09-28 09:17:31 +0200

    entrar

---
+++ ae1/main.js
@@ -0,0 +1,5 @@
+let nombres=['ana', 'javi', 'roberto', 'vanessa'];
+let numeros=[2,5,7,10];
+
+let newNumeros=array.map(numeros,nombres);
+console.log(newNumeros);
--- eje1/main.js
+++
@@ -1,7 +0,0 @@
-function nombres(x) {
-}
-
-let array = [{'name':'ana'},{'name':'javi'},{'name':'roberto'},{'name':'vanessa'}];
-array.forEach(nombres);
-console.log(array[0]);
-console.log(array[1]);
